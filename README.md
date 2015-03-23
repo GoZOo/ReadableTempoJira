6d26c57c84b90461a498b0161b4a76c95d965eca
GoZ <goz@226961.no-reply.drupal.org>
2015-03-23 10:53:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ReadableTempoJira
+
+Time float display isn't the best way to quickly read time. So this tampermonkey script add hours and minutes to each time value.
---
+++ ReadableTempoJira.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Add readable time to Jira
+// @namespace    https://github.com/GoZOo
+// @version      0.1
+// @description  Display readable hours minutes time to Tempo Jira extension
+// @author       Fabien Clément
+// @match        https://*.atlassian.net/secure/TempoUserBoard!timesheet.jspa?*
+// @grant        none
+// ==/UserScript==
+(function( $ ) {
+ 
+    $.fn.readableTempoJira = function() {
+        
+        var isNumber = function(n) {
+          return !isNaN(parseFloat(n)) && isFinite(n);
+        }
+        
+        var convertToReadable = function(n) {
+          n = Math.abs(n); // Change to positive
+          var hours = Math.floor(n);
+          var decimal = n - hours;
+          var minutes = Math.round(60 * decimal);
+          var readable = "";
+            if (hours > 0) {
+              readable = hours + "h";
+            }
+            if (minutes > 0) {
+              if (hours > 0) {
+                readable += " ";
+              }
+              readable += minutes + "min"
+            }
+            return readable;
+        }
+ 
+        return this.each(function() {
+            var cell = $( this );
+            var time_number = cell.text().trim();
+            if (isNumber(time_number)) {
+                var readable_time = convertToReadable(time_number);
+                if (readable_time) {
+                  cell.html(time_number + "<br />(" + readable_time + ")");
+                }
+            }
+        });
+ 
+    };
+ 
+}( jQuery ));
+
+jQuery(document).ready(function() {
+  jQuery("td.value-cell").readableTempoJira();
+  jQuery("td.hours").readableTempoJira();
+  jQuery("table.total-work-bottom td").readableTempoJira();
+  jQuery("table#issuetable > tfoot th").readableTempoJira();
+});
